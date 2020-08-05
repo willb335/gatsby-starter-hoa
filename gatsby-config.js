1b0e9610fddbe0e713fb22936b23183eff46a44d
Will Bashelor <wbashelor@gmail.com>
2020-08-05 06:22:41 -0400

    Set up Contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+
 /**
  * Configure your Gatsby site with this file.
  *
@@ -5,6 +7,13 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+  ],
 }
