509f793b5afc39454645fdad9a21c59bbe8fca3d
Will Bashelor <wbashelor@gmail.com>
2020-08-06 17:29:54 -0400

    Work with header spacing

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,12 @@
 import React from "react";
 import { Link, graphql, useStaticQuery } from "gatsby";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import styled from "styled-components";
+
+const StyledListItem = styled.li`
+  list-style-type: none;
+`;
 
 function Header() {
   const data = useStaticQuery(graphql`
@@ -15,25 +22,44 @@ function Header() {
 
   return (
     <header>
-      <h1>
-        <Link to="/">{title}</Link>
-      </h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/community">Community</Link>
-          </li>
-          <li>
-            <Link to="/calendar">Calendar</Link>
-          </li>
-          <li>
-            <Link to="/news">News</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+      <Grid container spacing={3} alignItems="center" justify="space-around">
+        <Grid item xs={4}>
+          <Paper variant="outlined">
+            <h1>
+              <Link to="/">{title}</Link>
+            </h1>
+          </Paper>
+        </Grid>
+        <Grid item xs={8}>
+          <nav>
+            <ul>
+              <Grid container spacing={1} justify="space-around">
+                <Grid item xs={4} />
+                <Grid item xs={2}>
+                  <StyledListItem>
+                    <Link to="/community">Community</Link>
+                  </StyledListItem>
+                </Grid>
+                <Grid item xs={2}>
+                  <StyledListItem>
+                    <Link to="/calendar">Calendar</Link>
+                  </StyledListItem>
+                </Grid>
+                <Grid item xs={2}>
+                  <StyledListItem>
+                    <Link to="/news">News</Link>
+                  </StyledListItem>
+                </Grid>
+                <Grid item xs={2}>
+                  <StyledListItem>
+                    <Link to="/contact">Contact</Link>
+                  </StyledListItem>
+                </Grid>
+              </Grid>
+            </ul>
+          </nav>
+        </Grid>
+      </Grid>
     </header>
   );
 }
