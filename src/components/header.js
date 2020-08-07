f5099f50aad6ecc162a9125a22ad54c43dd1f428
Will Bashelor <wbashelor@gmail.com>
2020-08-07 05:14:24 -0400

    Work with header

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,7 @@ import { Link, graphql, useStaticQuery } from "gatsby";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import styled from "styled-components";
+import Typography from "@material-ui/core/Typography";
 
 const StyledListItem = styled.li`
   list-style-type: none;
@@ -20,6 +21,29 @@ const StyledHeader = styled.header`
   margin-bottom: 30px;
 `;
 
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #5f6c80;
+
+  &:hover {
+    text-decoration: underline;
+    color: #000000;
+  }
+`;
+
+const StyledHomeLink = styled(Link)`
+  text-decoration: none;
+`;
+
+const StyledTypography = styled(Typography)`
+  color: #5f6c80;
+  transition: color 0.5s;
+
+  &:hover {
+    color: #000000;
+  }
+`;
+
 function Header() {
   const data = useStaticQuery(graphql`
     query {
@@ -34,36 +58,57 @@ function Header() {
 
   return (
     <StyledHeader>
-      <StyledPaper elevation={2}>
+      <StyledPaper elevation={0}>
         <Grid container spacing={3} alignItems="center">
           <Grid item xs={4}>
             <StyledTitle>
-              <Link to="/">{title}</Link>
+              <StyledHomeLink to="/">
+                <Typography variant="h4" color="textPrimary">
+                  {title}
+                </Typography>
+              </StyledHomeLink>
             </StyledTitle>
           </Grid>
           <Grid item xs={8}>
             <nav>
               <ul>
-                <Grid container spacing={1} justify="space-evenly">
-                  <Grid item xs={2} />
+                <Grid container spacing={0} justify="space-between">
                   <Grid item xs={2}>
                     <StyledListItem>
-                      <Link to="/community">Community</Link>
+                      <StyledLink to="/community">
+                        <StyledTypography variant="h6" color="secondary">
+                          Community
+                        </StyledTypography>
+                      </StyledLink>
                     </StyledListItem>
                   </Grid>
                   <Grid item xs={2}>
                     <StyledListItem>
-                      <Link to="/calendar">Calendar</Link>
+                      <StyledLink to="/calendar">
+                        <StyledTypography variant="h6" color="secondary">
+                          Calendar{" "}
+                        </StyledTypography>
+                      </StyledLink>
                     </StyledListItem>
                   </Grid>
                   <Grid item xs={2}>
                     <StyledListItem>
-                      <Link to="/news">News</Link>
+                      <StyledLink to="/news">
+                        <StyledTypography variant="h6" color="secondary">
+                          News
+                          {"     "}
+                        </StyledTypography>
+                      </StyledLink>
                     </StyledListItem>
                   </Grid>
                   <Grid item xs={2}>
                     <StyledListItem>
-                      <Link to="/contact">Contact</Link>
+                      <StyledLink to="/contact">
+                        <StyledTypography variant="h6" color="secondary">
+                          Contact
+                          {"  "}
+                        </StyledTypography>
+                      </StyledLink>
                     </StyledListItem>
                   </Grid>
                 </Grid>
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,39 @@
 import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import {
+  ThemeProvider,
+  createMuiTheme,
+  StylesProvider,
+} from "@material-ui/core/styles";
 
 import Header from "./header";
 import Footer from "./footer";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#2d3748",
+    },
+    secondary: {
+      main: "#5f6c80",
+    },
+    text: {
+      primary: "#000000",
+      secondary: "#2d3748",
+    },
+  },
+});
+
 function Layout({ children }) {
   return (
-    <>
-      <CssBaseline />
-      <Header />
-      {children}
-      <Footer />
-    </>
+    <StylesProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header />
+        {children}
+        <Footer />
+      </ThemeProvider>
+    </StylesProvider>
   );
 }
 
