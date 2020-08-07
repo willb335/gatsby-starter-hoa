c94289683812b2e9ee33184e8b04d73cd91123a1
Will Bashelor <wbashelor@gmail.com>
2020-08-07 11:10:47 -0400

    Implement carousel

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,50 +1,34 @@
 import React from "react";
-import { red, blue, green } from "@material-ui/core/colors";
-import Button from "@material-ui/core/Button";
-import { AutoRotatingCarousel } from "material-auto-rotating-carousel";
+import ReactCarousel, { Dots } from "@brainhubeu/react-carousel";
+
+import "@brainhubeu/react-carousel/lib/style.css";
 
 class Carousel extends React.Component {
-  state = { open: false };
+  state = { value: 0 };
+
+  onchange = value => {
+    this.setState({ value });
+  };
+
+  onClick = e => console.log(e);
 
   render() {
     return (
-      <div style={{ position: "relative", width: "100%", height: 500 }}>
-        <Button onClick={() => this.setState({ open: true })}>
-          Open carousel
-        </Button>
-        <AutoRotatingCarousel
-          autoplay={false}
-          open={this.state.open}
-          onClose={() => this.setState({ open: false })}
-          onStart={() => this.setState({ open: false })}
-          style={{ position: "absolute" }}
+      <>
+        <ReactCarousel
+          value={this.state.value}
+          onChange={this.onchange}
+          onClick={this.onClick}
+          plugins={["infinite"]}
         >
-          {/* <Slide
-            media={<img src='http://www.icons101.com/icon_png/size_256/id_79394/youtube.png' />}
-            mediaBackgroundStyle={{ backgroundColor: red[400] }}
-            style={{ backgroundColor: red[600] }}
-            title='This is a very cool feature'
-            subtitle='Just using this will blow your mind.'
-          />
-          <Slide
-            media={<img src='http://www.icons101.com/icon_png/size_256/id_80975/GoogleInbox.png' />}
-            mediaBackgroundStyle={{ backgroundColor: blue[400] }}
-            style={{ backgroundColor: blue[600] }}
-            title='Ever wanted to be popular?'
-            subtitle='Well just mix two colors and your are good to go!'
-          />
-          <Slide
-            media={<img src='http://www.icons101.com/icon_png/size_256/id_76704/Google_Settings.png' />}
-            mediaBackgroundStyle={{ backgroundColor: green[400] }}
-            style={{ backgroundColor: green[600] }}
-            title='May the force be with you'
-            subtitle='The Force is a metaphysical and ubiquitous power in the Star Wars fictional universe.'
-          /> */}
-          <div style={{ zIndex: 10 }}>Hello World</div>
-          <div>Hello World2</div>
-          <div>Hello World3</div>
-        </AutoRotatingCarousel>
-      </div>
+          {this.props.children}
+        </ReactCarousel>
+        <Dots
+          value={this.state.value}
+          onChange={this.onchange}
+          number={this.props?.children?.length}
+        />
+      </>
     );
   }
 }
--- src/pages/community.js
+++ src/pages/community.js
@@ -2,10 +2,19 @@ import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
 import Img from "gatsby-image";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import styled from "styled-components";
 import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 
 import Layout from "../components/layout";
 import Carousel from "../components/carousel";
+
+const StyledImg = styled(Img)`
+  width: 100%;
+`;
+const StyledPaper = styled(Paper)`
+  width: 75%;
+`;
 
 export default function Home() {
   const data = useStaticQuery(graphql`
@@ -39,8 +48,9 @@ export default function Home() {
     <Layout>
       <Grid
         container
-        spacing={3}
+        spacing={1}
         alignItems="center"
+        justify="center"
         style={{ paddingLeft: "2vw", paddingRight: "2vw" }}
       >
         <Grid item xs={12} md={12}>
@@ -49,15 +59,19 @@ export default function Home() {
         <Grid item xs={12} md={12}>
           <Carousel />
         </Grid>
-        {photos.map((photo, i) => {
-          const { fluid } = photo.node.childImageSharp;
-          const { id } = photo.node;
-          return (
-            <Grid item xs={12} md={12}>
-              <Img key={id} fluid={fluid} fadeIn alt="i"></Img>
-            </Grid>
-          );
-        })}
+        <Grid item xs={12} md={12}>
+          <Carousel>
+            {photos.map((photo, i) => {
+              const { fluid } = photo.node.childImageSharp;
+              const { id } = photo.node;
+              return (
+                <StyledPaper key={id} elevation={3}>
+                  <StyledImg fluid={fluid} fadeIn alt="i"></StyledImg>
+                </StyledPaper>
+              );
+            })}
+          </Carousel>
+        </Grid>
         <Grid item xs={12} md={12}>
           <h2>{title}</h2>
         </Grid>
