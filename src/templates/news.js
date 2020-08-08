8f4716945fad7ede6ab4746cd5f438ae80f2f9d1
Will Bashelor <wbashelor@gmail.com>
2020-08-08 15:22:29 -0400

    Fix material ui pagination

--- src/templates/news.js
+++ src/templates/news.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Link, graphql, useStaticQuery } from "gatsby";
+import { Link, graphql } from "gatsby";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import styled from "styled-components";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
 import Pagination from "@material-ui/lab/Pagination";
+import PaginationItem from "@material-ui/lab/PaginationItem";
 
 import Layout from "../components/layout";
 
@@ -54,45 +54,35 @@ export const query = graphql`
   }
 `;
 
-function News(props) {
-  console.log("props", props);
-  // console.log("pageContext", pageContext, "data", data, loading, "loading");
-  const [page, setPage] = React.useState(1);
-  // const data = useStaticQuery(graphql`
-  //   query($skip: Int!, $limit: Int!) {
-  //     allContentfulNews(
-  //       sort: { fields: datePublished, order: DESC }
-  //       limit: $limit
-  //       skip: $skip
-  //     ) {
-  //       articles: edges {
-  //         article: node {
-  //           title
-  //           slug
-  //           datePublished(formatString: "MMMM Do, YYYY")
-  //           author
-  //           id
-  //           mainImage {
-  //             description
-  //             fixed(width: 150, height: 150, quality: 100) {
-  //               ...GatsbyContentfulFixed_withWebp
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // `);
+function News({ pageContext, data, location }) {
+  console.log("pageContext", pageContext);
+  console.log("location", location);
 
-  const { previousPagePath, nextPagePath } = props.pageContext;
+  const {
+    previousPagePath,
+    nextPagePath,
+    numberOfPages,
+    humanPageNumber,
+  } = pageContext;
 
-  const handlePaginationChange = (e, value) => {
-    console.log(value);
+  const handlePagePath = item => {
+    if (item.type === "previous") {
+      if (previousPagePath === "") return "/";
+      return previousPagePath;
+    }
 
-    setPage(value);
+    if (item.type === "next") {
+      if (nextPagePath === "") return "/";
+      return nextPagePath;
+    }
+
+    if (item.type === "page") {
+      if (item.page === 1) return `/news`;
+      return `/news/${item.page}`;
+    }
   };
 
-  const { articles } = props.data?.allContentfulNews;
+  const { articles } = data?.allContentfulNews;
 
   return (
     <Layout>
@@ -127,13 +117,19 @@ function News(props) {
             })}
           </ul>
           <Grid item xs={12}>
-            {previousPagePath && <Link to={previousPagePath}>Previous</Link>}
-            {nextPagePath && <Link to={nextPagePath}>Next</Link>}
-            {/* <Pagination
-              page={page}
-              onChange={handlePaginationChange}
-              count={Math.ceil(articles?.length / 5)}
-            ></Pagination> */}
+            <Pagination
+              page={humanPageNumber}
+              renderItem={item =>
+                console.log("item", handlePagePath(item)) || (
+                  <PaginationItem
+                    component={Link}
+                    to={handlePagePath(item)}
+                    {...item}
+                  />
+                )
+              }
+              count={numberOfPages}
+            ></Pagination>
           </Grid>
         </Grid>
       </Grid>
