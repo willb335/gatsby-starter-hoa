ea4c33a6f2826a4e5f127df2f13c5ddccde433e0
Will Bashelor <wbashelor@gmail.com>
2020-08-10 08:59:50 -0400

    Add setup script

--- src/components/header.js
+++ src/components/header.js
@@ -62,7 +62,7 @@ function Header() {
       <Grid container spacing={3} alignItems="center">
         <Grid item xs={4}>
           <StyledHomeLink to="/">
-            <Typography variant="h4" color="primary" style={{ padding: 10 }}>
+            <Typography variant="h4" color="primary" style={{ padding: 20 }}>
               {title}
             </Typography>
           </StyledHomeLink>
